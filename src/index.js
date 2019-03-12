c90cc302ec21808815303fd67b18eeb7025acd8c
Rupert Ong <rupert.ong@gmail.com>
2019-03-12 15:17:29 -0400

    Add validityState polyfill

--- src/index.js
+++ src/index.js
@@ -188,6 +188,113 @@ document.addEventListener('submit', function(e) {
   }
 });
 
+/* Validity Polyfill */
+(function(window, document, undefined) {
+  var supported = function() {
+    var input = document.createElement('input');
+    return (
+      'validity' in input &&
+      'badInput' in input.validity &&
+      'patternMismatch' in input.validity &&
+      'rangeOverflow' in input.validity &&
+      'stepMismatch' in input.validity &&
+      'tooLong' in input.validity &&
+      'tooShort' in input.validity &&
+      'typeMismatch' in input.validity &&
+      'valid' in input.validity &&
+      'valueMissing' in input.validity
+    );
+  };
+
+  var getValidityState = function(field) {
+    var type = field.getAttribute('type') || field.nodeName.toLowerCase(),
+      isNum = type === 'number' || type === 'range',
+      length = field.value.length;
+
+    if (field.type === 'radio' && field.name) {
+      var group = document.getElementsByName(field.name);
+      if (group.length > 0) {
+        var i = 0;
+        for (; i < group.length; i++) {
+          if (group[i].form === field.form && field.checked) {
+            field = group[i];
+            break;
+          }
+        }
+      }
+    }
+
+    var valid = true,
+      checkValidity = {
+        badInput: isNum && length > 0 && !/[+-]?[0-9]/.test(field.value),
+        patternMismatch:
+          field.hasAttribute('pattern') &&
+          length > 0 &&
+          new RegExp(field.getAttribute('pattern').test(field.value) === false),
+        rangeOverflow:
+          field.hasAttribute('max') &&
+          isNum &&
+          field.value > 1 &&
+          parseInt(field.value, 10) > parseInt(field.getAttribute('max'), 10),
+        rangeUnderflow:
+          field.hasAttribute('min') &&
+          isNum &&
+          field.value > 1 &&
+          parseInt(field.value, 10) < parseInt(field.getAttribute('min'), 10),
+        stepMismatch:
+          field.hasAttribute('step') &&
+          field.getAttribute('step') !== 'any' &&
+          isNum &&
+          Number(field.value) % parseFloat(field.getAttribute('step')) !== 0,
+        tooLong:
+          field.hasAttribute('maxLength') &&
+          field.getAttribute('maxLength') > 0 &&
+          length > parseInt(field.getAttribute('maxLength'), 10),
+        tooShort:
+          field.hasAttribute('minLength') &&
+          field.getAttribute('minLength') > 0 &&
+          length > 0 &&
+          length < parseInt(field.getAttribute('minLength'), 10),
+        typeMismatch:
+          length > 0 &&
+          ((type === 'email' &&
+            !/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(
+              field.value
+            )) ||
+            (type === 'url' &&
+              !/^(?:(?:https?|HTTPS?|ftp|FTP):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]-*)*[a-zA-Z\u00a1-\uffff0-9]+)*)(?::\d{2,5})?(?:[\/?#]\S*)?$/.test(
+                field.value
+              ))),
+        valueMissing:
+          field.hasAttribute('required') &&
+          (((type === 'checkbox' || type === 'radio') && !field.checked) ||
+            (type === 'select' && field.options[field.selectedIndex].value < 1) ||
+            (type !== 'checkbox' && type !== 'radio' && type !== 'select' && length < 1))
+      };
+
+    for (var key in checkValidity) {
+      if (checkValidity.hasOwnProperty(key)) {
+        if (checkValidity[key]) {
+          valid = false;
+          break;
+        }
+      }
+    }
+
+    checkValidity.valid = valid;
+    return checkValidity;
+  };
+
+  if (!supported) {
+    Object.defineProperty(HTMLInputElement.prototype, 'validity', {
+      get: function ValidityState() {
+        return getValidityState(this);
+      },
+      configurable: true
+    });
+  }
+})(window, document);
+
 /* Toggle Input states */
 document.querySelector('._toggleDisable').addEventListener(
   'click',
@@ -221,6 +328,17 @@ document.querySelector('._toggleError').addEventListener(
       var elem = inputContainers[i];
       elem.classList.toggle('mdwc-text-field--error');
     }
+  },
+  false
+);
+
+document.querySelector('._setNameField').addEventListener(
+  'click',
+  function(e) {
+    var field = document.querySelector('#myText');
+    field.value = 'Sherlock Holmes';
+    field.focus();
+    field.blur();
   },
   false
 );
